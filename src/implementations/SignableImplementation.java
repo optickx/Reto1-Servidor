cd671ab0095385241f762020c4253ac3aab0317f
Epeldex <alexander.epelde@gmail.com>
2023-10-20 14:17:55 +0200

    Sign in method + all the sql definitions

--- src/implementations/SignableImplementation.java
+++ src/implementations/SignableImplementation.java
@@ -2,7 +2,9 @@ package implementations;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
 import exceptions.BadCredentialsException;
 import exceptions.NoSuchUserException;
 import exceptions.ServerCapacityException;
@@ -20,20 +22,65 @@ public class SignableImplementation implements Signable {
     @Override
     public User signIn(User user)
             throws BadCredentialsException, NoSuchUserException, ServerCapacityException, ServerErrorException {
+        User newUser = user;
         con = Pool.getConnection();
-    
-        try {
-            stmt = con.prepareStatement();
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        if (userExists(user))
+            newUser = getUser(checkPassword(user));
 
-        return null;
+        return newUser;
     }
 
     @Override
     public void signUp(User user) throws ServerCapacityException, ServerErrorException {
     }
 
+    private boolean userExists(User user) throws NoSuchUserException, ServerErrorException {
+        try {
+            stmt = con.prepareStatement(LOGIN_CHECK);
+            stmt.setString(1, user.getEmail());
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next())
+                return true;
+            else
+                throw new NoSuchUserException("User does not exist");
+        } catch (SQLException e) {
+            throw new ServerErrorException("Error checking login mail");
+        }
+    }
+
+    private int checkPassword(User user) throws BadCredentialsException, ServerErrorException {
+        try {
+            stmt = con.prepareStatement(PASSWORD_CHECK);
+            stmt.setString(1, user.getEmail());
+            stmt.setString(2, user.getPassword());
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next())
+                return rs.getInt("id");
+            else
+                throw new BadCredentialsException("Incorrect password");
+        } catch (SQLException e) {
+            throw new ServerErrorException("Error checking for password");
+        }
+    }
+
+    private User getUser(int userId) throws ServerErrorException {
+        User user = new User();
+        try {
+            stmt = con.prepareStatement(USER_SELECT);
+            stmt.setInt(1, userId);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                user.setEmail(rs.getString("login"));
+                user.setPassword(rs.getString("password"));
+                user.setFullName(rs.getString("name"));
+                user.setStreet(rs.getString("street"));
+                user.setPostalCode(rs.getInt("zip"));
+                user.setCity(rs.getString("city"));
+                user.setPhone(rs.getString("phone"));
+            }
+        } catch (SQLException e) {
+            throw new ServerErrorException("Error getting user's information");
+        }
+        return user;
+    }
 }
--- src/implementations/SqlDefinitions.java
+++ src/implementations/SqlDefinitions.java
@@ -7,26 +7,33 @@ package implementations;
  * @author Alexander Epelde
  */
 public abstract class SqlDefinitions {
-    /**
-     * Selects everything you need to know about a said user, only able to be used
-     * after verifying a users identity
-     */
-    public static final String COMPLETE_USER_SELECT = "SELECT ru.login, ru.password, rp.name, rp.street, rcs.name as state, rp.zip, rp.city, rc.name as country, rp.phone "
-            + "JOIN public.res_users ru on ru.partner_id = rp.id "
-            + "JOIN public.res_country rc on rp.country_id = rc.id "
-            + "JOIN public.res_country_state rcs on rp.state_id = rcs.id " + "WHERE ru.id =?";
+        /**
+         * Selects everything you need to know about a said user, only able to be used
+         * after verifying a users identity with both LOGIN_CHECK and PASSWORD_CHECK in
+         * that order
+         */
+        public static final String USER_SELECT = "SELECT ru.login, ru.password, rp.name, rp.street, rcs.name as state, rp.zip, rp.city, rc.name as country, rp.phone "
+                        + "JOIN public.res_users ru on ru.partner_id = rp.id "
+                        + "JOIN public.res_country rc on rp.country_id = rc.id "
+                        + "JOIN public.res_country_state rcs on rp.state_id = rcs.id " + "WHERE ru.id =?";
 
-    /**
-     * Selects user id from an existing user.
-     * Intended use: to know if the users' password is correct
-     */
-    public static final String USER_ID_SELECT = "SELECT id FROM public.res_users"
-            + "WHERE login = ? and password = ?";
+        /**
+         * Selects user id from an existing user.
+         * Intended use: to know if the users' password is correct
+         */
+        public static final String PASSWORD_CHECK = "SELECT id FROM public.res_users "
+                        + "WHERE login = ? and password = ?";
 
-    /**
-     * Selects user id from received user.
-     * Intended use: to check if the user exists
-     */
-    public static final String USER_SELECT = "SELECT id FROM public.res_users"
-            + "WHERE login = ?";
+        /**
+         * Selects user id from received user.
+         * Intended use: to check if the user exists
+         */
+        public static final String LOGIN_CHECK = "SELECT id FROM public.res_users "
+                        + "WHERE login = ?";
+
+        /**
+         * Calls a procedure that handles the insertion of a new user
+         * The fields to fill are in the same order as the User class
+         */
+        public static final String INSERT_NEW_USER = "CALL insert_new_user(?,?,?,?,?,?,?,?,?)";
 }
